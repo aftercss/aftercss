82f73044aeb0dd4f7f9374e11c6693507129463b
zoolsher <zoolsher@gmail.com>
2018-09-22 17:53:15 +0800

    WIP

--- packages/aftercss-tokenizer/src/token.ts
+++ packages/aftercss-tokenizer/src/token.ts
@@ -1,7 +1,10 @@
+// tslint:disable max-classes-per-file
 export enum TokenType {
+  COMMENT = 'COMMENT',
   EOF = 'EOF',
+  NEWLINE = 'NEWLINE',
   ANY = 'ANY',
-  IDENT = 'IDENT',
+  IDENT = 'IDENT', // NO
   ATKEYWORD = 'ATKEYWORD',
   STRING = 'STRING',
   BAD_STRING = 'BAD_STRING',
@@ -16,8 +19,7 @@ export enum TokenType {
   CDO = 'CDO',
   CDC = 'CDC',
   COLON = 'COLON',
-  SEMI = 'SEMI',
-  COMMENT = 'COMMENT'
+  SEMI = 'SEMI'
 }
 
 export class Token {
@@ -25,17 +27,48 @@ export class Token {
   /**
    * maybe undefined will save some Memory than ''
    */
-  public content: string = undefined;
-  public constructor(type: TokenType, content?: string) {
+  public raw: string = undefined;
+  public constructor(type: TokenType, raw?: string) {
     this.type = type;
-    if (content) {
-      this.content = content;
+    if (raw) {
+      this.raw = raw;
     }
   }
 }
+/**
+ * New Line
+ */
+enum NewLineTokenType {
+  RN = 'RN',
+  R = 'R',
+  N = 'N',
+  F = 'F'
+}
+export class NewLineToken extends Token {
+  public endType: NewLineTokenType;
+  public constructor(type: TokenType, raw?: string) {
+    super(type, raw);
+    switch (raw) {
+      case '\r\n':
+        this.endType = NewLineTokenType.RN;
+      case '\n':
+        this.endType = NewLineTokenType.N;
+      case '\r':
+        this.endType = NewLineTokenType.R;
+      case '\f':
+        this.endType = NewLineTokenType.F;
+      default:
+        throw new Error(`unexpect ${raw} for NewLineTokenType`);
+    }
+  }
+}
+
 function TokenFactory(type: TokenType.EOF): Token;
-function TokenFactory(type: TokenType.COMMENT, content: string): Token;
+function TokenFactory(type: TokenType.COMMENT | TokenType.NEWLINE, content: string): Token;
 function TokenFactory(type: TokenType, content?: string): Token {
+  if (type === TokenType.NEWLINE) {
+    return new NewLineToken(type, content);
+  }
   return new Token(type, content);
 }
 
--- packages/aftercss-tokenizer/src/tokenizer.ts
+++ packages/aftercss-tokenizer/src/tokenizer.ts
@@ -1,5 +1,7 @@
 import { TokenFactory, TokenType } from './token';
 
+const WhiteSpaceRegex = /\s+/;
+
 class Tokenizer {
   public content: string = '';
   private end: number = this.content.length;
@@ -9,31 +11,63 @@ class Tokenizer {
     this.content = content;
     this.end = this.content.length;
   }
+  public error(message: string) {
+    throw new Error(message);
+  }
   /**
    * readUntil `s` show up
    * @param s
    */
-  public readUntil(s: string) {
-    const nextPosition = this.content.indexOf(s, this.current);
-    const value = s.slice(this.current, nextPosition);
-    this.current = nextPosition;
-    return value;
+  public readUntil(pattern: RegExp) {
+    if (this.current >= this.content.length) {
+      this.error(
+        JSON.stringify({
+          code: `unexpected-eof`,
+          message: 'Unexpected end of input'
+        })
+      );
+    }
+    const start = this.current;
+    const match = pattern.exec(this.content.slice(start));
+
+    if (match) {
+      this.current = start + match.index;
+      return this.content.slice(start, this.current);
+    }
+
+    this.current = this.content.length;
+    return this.content.slice(start);
+  }
+  /**
+   * match
+   * @param str
+   */
+  public match(str: string) {
+    return this.content.slice(this.current, this.current + str.length) === str;
   }
   /**
    * eat
    */
-  public eat(str?: string, error: boolean = false) {
-    if (!str) {
-      return this.content.charAt(this.current++);
+  public eat(str: string, required: boolean = false, message?: string) {
+    if (this.match(str)) {
+      this.current += str.length;
+      return true;
     }
-    const len = str.length;
-    const slice = this.content.slice(this.current, this.current + len);
-    if (str === slice) {
-      return slice;
-    } else {
-      if (error !== false) {
-        throw new Error(`expecting ${str} but get ${slice}`);
-      }
+
+    if (required) {
+      this.error(
+        JSON.stringify({
+          code: `unexpected-${this.current === this.content.length ? 'eof' : 'token'}`,
+          message: message || `Expected ${str}`
+        })
+      );
+    }
+    return false;
+  }
+
+  public allowWhitespace() {
+    while (this.current < this.content.length && WhiteSpaceRegex.test(this.content[this.current])) {
+      this.current++;
     }
   }
   /**
@@ -46,38 +80,38 @@ class Tokenizer {
    * CSS3 defined input process https://www.w3.org/TR/css-syntax-3/#input-preprocessing
    * preprocess CSSChar
    */
-  public preprocess(): string {
-    const char = this.eat();
-    if (char === '\u000C' || char === '\u000D') {
-      return '\u000A';
-    }
-    if (char === '\u000A' && this.pick() === '\u000D') {
-      // eat that pick
-      this.eat();
-      return '\u000A';
-    }
-    if (char === '\u0000') {
-      return '\uFFFD';
-    }
+  public preprocess() {
+    // TODO: bad performance. fixable in stream.
+    // TODO: and this hurts sourcemap.
+    this.content.replace('\u000A\u000D', '\u000A');
+    this.content.replace('\u000C', '\u000A');
+    this.content.replace('\u000D', '\u000A');
+    this.content.replace('\u0000', '\uFFFD');
   }
-  public readChar() {
-    return this.preprocess();
-  }
+
   /**
    * 生成Token用的
    */
-  public getToken() {
+  public nextToken() {
     if (this.current >= this.end) {
       return TokenFactory(TokenType.EOF);
     }
-    const char = this.readChar();
     /**
+     * comment token
      * https://www.w3.org/TR/css-syntax-3/#comment-diagram
      */
-    if (char === '/') {
-      if (this.pick() === '*') {
-        this.eat('*');
-        return TokenFactory(TokenType.COMMENT, '');
+    if (this.eat('/*')) {
+      const commentContent = this.readUntil(/\*\//);
+      return TokenFactory(TokenType.COMMENT, commentContent);
+    }
+    /**
+     * new line token
+     * https://www.w3.org/TR/css-syntax-3/#newline-diagram
+     */
+    const newlineList = ['\r\n', '\n', '\r', '\f'];
+    for (const item in newlineList) {
+      if (this.eat(item)) {
+        return TokenFactory(TokenType.NEWLINE, item);
       }
     }
   }
