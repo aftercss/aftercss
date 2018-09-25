f22a9907945a653cafab8889defc9d74cb60a0f3
zoolsher <zoolsher@gmail.com>
2018-09-25 22:20:18 +0800

    WIP

--- packages/test/base-framework/base-fixture.js
+++ packages/test/base-framework/base-fixture.js
@@ -1,12 +1,18 @@
 import fs from 'fs';
 import path from 'path';
+import { promisify } from 'util';
 
 const writeFileP = fs.promises.writeFile;
 const readFileP = fs.promises.readFile;
+const fileExistsP = promisify(fs.exists);
 
 export class BaseFixture {
   constructor(currentDir) {
     this.currentDir = currentDir;
+    this.srcDir = path.resolve(`${this.currentDir}/src/`);
+    this.expectDir = path.resolve(`${this.currentDir}/expect/`);
+    this.actualDir = path.resolve(`${this.currentDir}/actual/`);
+    this.errorDir = path.resolve(`${this.currentDir}/error/`);
   }
   isString(s) {
     return Object.prototype.toString.call(s) === '[object String]';
@@ -15,23 +21,32 @@ export class BaseFixture {
     if (!(this.isString(filename) && this.isString(content))) {
       return;
     }
-    const filePath = path.resolve(`${this.currentDir}/expect/${filename}`);
+    const filePath = path.resolve(`${this.expectDir}/${filename}`);
     return await writeFileP(filePath, content, { encoding: 'utf8', flag: 'w' });
   }
   async writeActualFile(filename, content) {
     if (!(this.isString(filename) && this.isString(content))) {
       return;
     }
-    const filePath = path.resolve(`${this.currentDir}/actual/${filename}`);
+    const filePath = path.resolve(`${this.actualDir}/${filename}`);
+    return await writeFileP(filePath, content, { encoding: 'utf8', flag: 'w' });
+  }
+  async writeErrorFile(content) {
+    if (this.isString(content)) {
+      return;
+    }
+    const filePath = path.resolve(`${this.errorDir}/index.json`);
     return await writeFileP(filePath, content, { encoding: 'utf8', flag: 'w' });
   }
   async readSrcDir(filename) {
-    const filePath = path.resolve(`${this.currentDir}/src/${filename}`);
+    const filePath = path.resolve(`${this.srcDir}/${filename}`);
     return readFileP(filePath, {
       encoding: 'utf8',
       flag: 'r',
     });
   }
+  async readActualDir() {}
+  async readExpectDir() {}
   async getAllDirs() {
     const files = await fs.promises.readdir(this.currentDir);
     const dirs = files.filter(async file => {
@@ -40,5 +55,41 @@ export class BaseFixture {
     });
     return dirs;
   }
-  async runTask() {}
+  async build() {
+    throw new Error('error should be emitted');
+  }
+  async compareError(error) {
+    const errorPath = path.resolve(`${this.currentDir}/error/index.json`);
+    const errorFileExists = await fileExistsP(errorPath);
+    if (!errorFileExists) {
+      this.writeErrorFile(
+        JSON.stringify(
+          {
+            message: error.message,
+          },
+          null,
+          2,
+        ),
+      );
+    }else{
+      const errorJSON = require(errorPath);
+      const errorMessage = errorJSON.message;
+      
+    }
+
+    try {
+      const errorJSON = require(errorPath);
+    } catch (e) {
+      throw new Error('');
+    }
+    const errorMessage = errorJSON.message;
+    if (e.message.indexOf(errorMessage) >= 0) {
+      return true;
+    }
+  }
+  async runTask() {
+    try {
+      await this.build();
+    } catch (e) {}
+  }
 }
